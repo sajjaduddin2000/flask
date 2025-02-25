cd56a0ee31ae2404651d9fccb346e810f8e6c20d
sajjaduddin2000 <sajjaduddin2000@outlook.com>
2025-02-24 22:27:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import os
-import uvicorn
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "FastAPI is running on Azure!"}
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Use PORT from Azure or default to 8000
-    uvicorn.run(app, host="0.0.0.0", port=port)
+return "Hello World! !"
